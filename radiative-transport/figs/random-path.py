8df4477c71537c57838e5873eab3e307b70fe9c6
Edward Brown <ebrown@pa.msu.edu>
2018-05-25 07:46:28 -0400

    illustration of random walk

--- radiative-transport/figs/random-path.py
+++ radiative-transport/figs/random-path.py
@@ -1,27 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def hop(r0,l=1.0):
-    ang = 2.0*np.pi*np.random.random()
-    return r0 + l*np.array([np.cos(ang),np.sin(ang)])
+def hop(n,l=1.0):
+    ang = 2.0*np.pi*np.random.random(n)
+    return np.array([l*np.cos(a) for a in ang]), np.array([l*np.sin(a) for a in ang])
 
 plt.style.use('text-sans')
+N_ARROWS=50
+hop_x,hop_y = hop(N_ARROWS)
+cum_x = hop_x.cumsum()
+cum_y = hop_y.cumsum()
+
+extent = max(np.abs(cum_x).max(),np.abs(cum_y).max())
+x = 0.0
+y = 0.0
 plt.figure(figsize=(2,2))
-NARROWS=20
+plt.xlim(-extent,extent)
+plt.ylim(-extent,extent)
+dalpha = 1/N_ARROWS
+alpha = dalpha
+for dx,dy in zip(hop_x,hop_y):
+    plt.arrow(x,y,dx,dy,
+        length_includes_head=True,
+        width=0.001,
+        color='black',
+        alpha = alpha)
+    x += dx
+    y += dy
+    alpha += dalpha
+plt.axis('off')
+plt.savefig('random-walk-schematic.pdf',format='pdf',bbox_inches='tight')
 
-
-if __name__ == "__main__":
-    r = np.zeros(2)
-    NHOPS = 1000
-    print('{0:5.2f}    {1:5.2f}'.format(r[0],r[1]))
-    x = np.zeros(NHOPS)
-    y = np.zeros(NHOPS)
-    for i in range(NHOPS):
-        r = hop(r)
-        x[i] = r[0]
-        y[i] = r[1]
-        if i % 20 == 0:
-            print('{0:5.2f}    {1:5.2f}'.format(r[0],r[1]))
-    print('\n\n')
-    print('{0:5.2f}    {1:5.2f}'.format(x.mean(),y.mean()))
-    print('{0:5.2f}    {1:5.2f}'.format(x.std(),y.std()))
