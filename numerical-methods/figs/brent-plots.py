8a3706141d93dfc63b6d361d319ab6c7895415bf
Edward Brown <ebrown@pa.msu.edu>
2020-04-23 13:05:28 -0400

    working versions of brent plots

--- numerical-methods/figs/brent-plots.py
+++ numerical-methods/figs/brent-plots.py
@@ -24,6 +24,16 @@ def bisect(f,a,b):
         b = m
     return a, b
 
+def secant(f,a,b):
+    """
+    Constructs secant to approximate root of f on a,b
+    """
+    fa = f(a)
+    fb = f(b)
+    if fa*fb >= 0.0:
+        raise ValueError("root not bracketed")
+    return (fb*a-fa*b)/(fb-fa)
+
 def inverse_quad(f,r,a,b):
     """
     Performs inverse quadratic interpolation to predict x s.t. f(x) = 0 in the interval
@@ -72,6 +82,50 @@ def quadfit(x,y):
         raise ValueError('unable to compute a, b, c')
     return (a,b,c)
 
+def brent_step(f,a,b,c,d,prev='bisection'):
+    delta = 4.0*np.finfo(1.0).eps
+    fa = f(a); fb = f(b); fc = f(c)
+    assert(fa*fb < 0)
+    assert(np.abs(fb) < np.abs(fa))
+    # propose a step
+    method = 'secant'
+    if (fa != fc) and (fb != fc):
+        s = inverse_quad(f,c,a,b)
+        method = 'inverse quadratic'
+    else:
+        s = secant(f,a,b)
+    # acceptance conditions
+    if (s > (3*a+b)/4 and s < b) or (s < (3*a+b)/4 and s > b):
+        if prev == 'bisection':
+            if np.abs(s-b) >= 0.5*np.abs(b-c) and np.abs(b-c) < delta:
+                s = 0.5*(a+b)
+                method = 'bisection'
+        else:
+            if np.abs(s-b) >= 0.5*np.abs(c-d) and np.abs(c-d) < delta:
+                s = 0.5*(a+b)
+                method = 'bisection'
+    else:
+        s = 0.5*(a+b)
+        method = 'bisection'
+    fs = f(s)
+    # set b[k-2] = b[k-1], b[k-1] = b[k]
+    d = c
+    c = b
+    # set b[k] based on sign of f(s) so that root remains bracketed
+    if fa*fs < 0:
+        # root lies between a, s so set b = s
+        b = s
+        fb = fs
+    else:
+        # root lies between s, b so move a to s
+        a = s
+        fa = fs
+    # keep b closer to root
+    if np.abs(fa) < np.abs(fb):
+        a,b = b,a
+    
+    return (a, b, c, d, s, method)
+    
 def base_plot(ax,x,y,color=LightGreyGreen,linewidth=0.5):
     '''
     Parameters
@@ -93,6 +147,63 @@ def base_plot(ax,x,y,color=LightGreyGreen,linewidth=0.5):
     ax.hlines(0,x.min(),x.max(),linestyle='-',linewidth=0.5,color=LightGreyGreen)
     return ax
 
+def sequence_plots(xleft,xright,Nsteps,basename='brent'):
+    rt = np.pi
+    x = np.linspace(xleft,xright,100)
+    y = f(x)
+    a = xleft; b = xright; c = a; d = 0.0
+    fa = f(a); fb = f(b)
+    assert(fa*fb < 0.0)
+    if np.abs(fa) < np.abs(fb):
+        a, b = b, a
+    for step in range(Nsteps):
+        print('step {0}'.format(step))
+        plotname = '{0}-{1:0d}.pdf'.format(basename,step)
+        fig = plt.figure(figsize=(2,2))
+        ax = fig.add_subplot(111)
+        ax = base_plot(ax,x,y,color=DarkGreyBlue,linewidth=1)
+        ax.plot([a,b],[f(a),f(b)],color=DarkGreyBlue,linestyle='none',marker='o',markersize=4)
+        xtks = [a,b]
+        ytks = [f(a),f(b)]
+        xlabs = [r'$a$',r'$b$']
+        ylabs = [r'$f(a)$',r'$f(b)$']
+        if c != a and c != b:
+            ax.plot(c,f(c),color=DarkGreyBlue,marker='o',markersize=4)
+            xtks += [c]
+            xlabs += [r'$c$']
+            ytks += [f(c)]
+            ylabs += [r'$f(c)$']
+        aa = a; bb = b; cc = c
+        fa = f(a); fb = f(b); fc = f(c)
+        a,b,c,d,s,method = brent_step(f,a,b,c,d)
+        print('method = {0}, s = {1:5.2f}'.format(method,s))
+        print('a, b, c, d = '+' '.join(['{0:5.2f}'.format(i) for i in (a,b,c,d)]))
+        ax.plot(s,f(s),color=Red,marker='o',markersize=4)
+        xtks += [s]
+        ytks += [f(s)]
+        if method != 'bisection':
+            xlabs += [r'$s$']
+            ylabs += [r'$f(s)$']
+        else:
+            xlabs += [r'$m$']
+            ylabs += [r'$f(m)$']            
+        # options
+        if method == 'secant':
+            ax.plot([aa,bb],[f(aa),f(bb)],color=Red,linestyle=':',linewidth=0.5)
+        elif method == 'inverse quadratic':
+            ypts = np.array([fa,fb,fc])
+            xpts = np.array([aa,bb,cc])
+            isort = np.argsort(ypts)
+            qa,qb,qc = quadfit(ypts[isort],xpts[isort])
+            yquad = np.linspace(y.min(),y.max(),50)
+            xquad = qa*yquad**2+qb*yquad+qc
+            ax.plot(xquad,yquad,color=Red,linestyle=':',linewidth=0.5)
+        ax.xaxis.set_ticks(xtks)
+        ax.xaxis.set_ticklabels(xlabs)
+        ax.yaxis.set_ticks(ytks)
+        ax.yaxis.set_ticklabels(ylabs)
+        fig.savefig(plotname,format='pdf',bbox_inches='tight')
+    
 if __name__=='__main__':
     import matplotlib.pyplot as plt
     from matplotlib import rc
@@ -101,51 +212,10 @@ if __name__=='__main__':
     plt.style.use('text-sans')
     rc('axes',titlesize='medium')
 
-    xguess = np.zeros(4)
+    xl = 1.6
+    xr = 5.1
+    sequence_plots(xl,xr,3)
 
-    rt = np.pi
-    xl = 1.5
-    xr = 4.0
-    xm = 0.5*(xl+xr)
-    xpts = np.array([xl,xm,xr])
-    ypts = f(xpts)
-    x = np.linspace(xl,xr,100)
-    y = f(x)
-    fig = plt.figure(figsize=(2,2))
-    ax = fig.add_subplot(111)
-    ax = base_plot(ax,x,y,color=DarkGreyBlue,linewidth=1.0)
-    a,b,c = quadfit(ypts,xpts)
-    xquad = a*y**2 + b*y + c
-    ax.plot(xpts,ypts,color=DarkGreyBlue,linestyle='none',marker='o')
-    ax.plot(xquad,y,color=DarkerGreen,linestyle=':',linewidth=0.5)
-    r = inverse_quad(f,xm,xl,xr)
-    ax.plot(r,f(r),color=Red,marker='o')
-    ax.vlines(r,f(r),0.0,color=Red,linestyle=':',linewidth=0.5)
-    xtks = np.array([xl,xm,rt,r,xr])
-    ytks = f(xtks)
-    ax.xaxis.set_ticks(xtks)
-    ax.xaxis.set_ticklabels([r'$a$',r'$m$',r'$r$',r'$g$',r'$b$'])
-    ax.yaxis.set_ticks(ytks)
-    ax.yaxis.set_ticklabels([r'$f(a)$',r'$f(m)$',r'0',r'$f(g)$',r'$f(b)$'])    
-    fig.savefig('brent-1.pdf',format='pdf',bbox_inches='tight')
-    
-    fig = plt.figure(figsize=(2,2))
-    ax = fig.add_subplot(111)
-    xpts = np.array([xm,r,xr])
-    ypts = f(xpts)
-    ax = base_plot(ax,x,y,color=DarkGreyBlue,linewidth=1.0)
-    a,b,c = quadfit(ypts,xpts)
-    xquad = a*y**2 + b*y + c
-    ax.plot(xpts,ypts,color=DarkGreyBlue,linestyle='none',marker='o')
-    ax.plot(xquad,y,color=DarkerGreen,linestyle=':',linewidth=0.5)
-    g = r
-    r = inverse_quad(f,g,xm,xr)
-    ax.plot(r,f(r),color=Red,marker='o')
-    xtks = np.array([xm,r,g,xr])
-    ytks = f(xtks)
-    ax.xaxis.set_ticks(xtks)
-    ax.xaxis.set_ticklabels([r'$m$',r'$r$',r'$g$',r'$b$'])
-    ax.yaxis.set_ticks(ytks)
-    ax.yaxis.set_ticklabels([r'$f(m)$',r'0',r'$f(g)$',r'$f(b)$'])    
-    fig.savefig('brent-2.pdf',format='pdf',bbox_inches='tight')    
-    
+    xl = 1.4
+    xr = 5.9
+    sequence_plots(xl,xr,3,basename='trial')
